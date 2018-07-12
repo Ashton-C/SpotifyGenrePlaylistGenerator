9c55e031e6db22057e723108456c0da274e92c98
Ashton Christensen <armadillomaster@gmail.com>
2018-07-11 20:12:08 -0600

    Figured out the key stuff

--- GraceNoteUserID.py
+++ GraceNoteUserID.py
@@ -5,8 +5,7 @@ userID2 = ''
 def main():
     clientID = '1984033224-5834A5143CE87D68376F48BF28A6BEE4'
     userID = pygn.register(clientID)
-    userID2 = userID
-    print(userID2)
+    print(userID)
 
 if __name__ == '__main__':
     main()
---
+++ config.py
@@ -0,0 +1,5 @@
+
+SPOTIPY_CLIENT_SECRET = ''
+scope = 'user-library-read, playlist-modify-public'
+SPOTIPY_CLIENT_ID = '00fdf944926a403eb976a8a8171b0e54'
+SPOTIPY_REDIRECT_URI = 'http://localhost/'
