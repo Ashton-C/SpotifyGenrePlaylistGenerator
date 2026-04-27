833acb05e65e38ae70b7d84a6c4cda19460c00a3
Ashton Christensen <armadillomaster@gmail.com>
2026-04-26 23:52:28 -0700

    Remove deadlock-tracker (moved to https://github.com/Ashton-C/DeadlockTracker)

--- deadlock-tracker/backend/app/routers/matches.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.services import deadlock_api
-
-router = APIRouter(prefix="/api/matches", tags=["matches"])
-
-
-@router.get("/{match_id}")
-async def get_match(match_id: int):
-    return await deadlock_api.get_match_metadata(match_id)
